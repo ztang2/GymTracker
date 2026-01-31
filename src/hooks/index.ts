e3373526d54a6904fe2b22c82f5782327119d25c
Zhuoran Tang <zhuorantang@Zhuorans-Mac-mini.local>
2026-01-31 11:21:33 -0800

    fix: re-export hooks from index.ts (useWorkoutTimer, useRestTimer, useWorkoutState)

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,6 @@
 // Export all hooks
 export { useExerciseSearch } from './useExerciseSearch';
 export type { UseExerciseSearchOptions, UseExerciseSearchResult } from './useExerciseSearch';
+export { useWorkoutTimer } from './useWorkoutTimer';
+export { useRestTimer } from './useRestTimer';
+export { useWorkoutState } from './useWorkoutState';
