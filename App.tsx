6905030522c354770716496d6d38244678820954
Zhuoran Tang <zhuorantang@Zhuorans-Mac-mini.local>
2026-01-31 12:32:41 -0800

    fix: add react-native-reanimated, gesture-handler, masked-view for native support

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+import 'react-native-reanimated';
 import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
